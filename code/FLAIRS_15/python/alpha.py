8f8b09b2bc3a6e2781c422df2be25e0978c553a9
yuxiao huang <yuxiaohuang@yuxiaos-Air.home>
2016-06-30 00:54:27 -0400

    short version of linalg

--- code/FLAIRS_15/python/alpha.py
+++ code/FLAIRS_15/python/alpha.py
@@ -70,51 +70,47 @@ alphabet_cont = []
 relations_Dic = {}
 
 # calculate causal significance for all relationships
-# @param        result_file         file containing relationships and their causal significance
-def get_all_alpha(result_file):
+# @param        alpha_file         file containing relationships and their causal significance
+def get_all_alpha(alpha_file):
   # output heading
-  with open(result_file, 'wb') as f:
+  with open(alpha_file, 'wb') as f:
     spamwriter = csv.writer(f, delimiter = ',')
     spamwriter.writerow(["cause", "effect", "window_start", "window_end", "alpha"])
 
   for e in relations_Dic:
-    if e == 'z':
-      get_alpha(e, result_file)
+    get_alpha(e, alpha_file)
 
 
 # calculate alpha for one relationship
 # @param        effect_L            (list e), a list of e
-# @param        result_file         file containing relationships and their causal significance
-def get_alpha(e, result_file):
+# @param        alpha_file         file containing relationships and their causal significance
+def get_alpha(e, alpha_file):
   # get the set of potential causes and remove duplicates
   X_L = relations_Dic[e]
   if X_L:
-    A_array = get_A_array(X_L)
-    rank = np.linalg.matrix_rank(A_array) 
-    if rank == len(A_array):
-      # if A_array is full rank, solve system of linear equations
-      B_array = get_B_array(X_L, e)
-      #A_array = [1, 1, 1, 2, 3, 4, 5, 6, 8]
-      #B_array = [3, 9, 19]
-      B_array = np.linalg.solve(A_array, B_array)
-      get_result_file(X_L, e, B_array, result_file)
+    A_LL = get_A_LL(X_L)
+    rank = np.linalg.matrix_rank(A_LL)
+    if rank == len(A_LL):
+      # if A_LL is full rank, solve system of linear equations
+      B_L = get_B_L(X_L, e)
+      B_L = np.linalg.solve(A_LL, B_L)
+      get_alpha_file(X_L, e, B_L, alpha_file)
     else:
-      # if A_array is not full rank, get the subsystem of linear equations
+      # if A_LL is not full rank, get the subsystem of linear equations
       X_LIS_L = get_X_LIS_L(X_L, e)
-      print X_LIS_L
       if X_LIS_L:
         # solve the subsystem of linear equations
-        A_LIS_array = get_A_array(X_LIS_L)
-        B_LIS_array = get_B_array(X_LIS_L, e)
-        B_LIS_array = np.linalg.solve(A_LIS_array, B_LIS_array)
-        get_result_file(X_LIS_L, e, B_LIS_array, result_file)
+        A_LIS_LL = get_A_LL(X_LIS_L)
+        B_LIS_array = get_B_L(X_LIS_L, e)
+        B_LIS_array = np.linalg.solve(A_LIS_LL, B_LIS_array)
+        get_alpha_file(X_LIS_L, e, B_LIS_array, alpha_file)
 
 
-# get A_array, the coefficient matrix of the system of linear equations
+# get A_LL, the coefficient matrix of the system of linear equations
 # @param        X_L                 the set of potential causes
-def get_A_array(X_L):
+def get_A_LL(X_L):
   n = len(X_L)
-  A_array = []
+  A_LL = []
   for i in range(n):
     row_L = []
     for j in range(n):
@@ -129,8 +125,8 @@ def get_A_array(X_L):
       denominator = N_e_c * (len_T_e - len(T_e_c_L))
       f_e_c_x = nominator / denominator
       row_L.append(f_e_c_x)
-    A_array.append(row_L)
-  return A_array
+    A_LL.append(row_L)
+  return A_LL
 
 
 # get *N_e_c_x_Dic*
@@ -208,142 +204,13 @@ def get_N_e_c(c_L):
   return N_e_c_Dic[(c, r, s)]
 
 
-# is A_array full rank?
-# @param        A_array             the coefficient matrix of the system of linear equations
-def is_full_rank(A_array):
-  n = int(math.sqrt(len(A_array)))
-  full_rank = True
-  # gaussian elimination
-  for k in range(n - 1):
-    # find the max row and switch it with row k
-    max_val = abs(A_array[k * n + k])
-    max_row = k
-    # find the max row
-    for i in range(k + 1, n):
-      if abs(A_array[i * n + k]) > max_val:
-        max_val = abs(A_array[ i * n + k])
-        max_row = i
-    print max_val
-    if max_val == 0:
-      full_rank = False
-      break
-    # switch it with row k
-    if k != max_row:
-      for j in range(k, n):
-        A_array[max_row * n + j], A_array[k * n + j] = A_array[k * n + j], A_array[max_row * n + j]
-    # division
-    denominator = A_array[k * n + k]
-    for j in range(k, n):
-      A_array[k * n + j] = A_array[k * n + j] / denominator
-    # subtraction
-    for i in range(k + 1, n):
-      multiplier = A_array[i * n + k]
-      for j in range(k, n):
-        A_array[i * n + j] -= A_array[k * n + j] * multiplier
-    for i in range(n):
-      val_L = []
-      for j in range(n):
-        val_L.append(A_array[i * n + j])
-      print val_L
-    print
-  print [full_rank, A_array[(n - 1) * n + n - 1]]
-  # return
-  if full_rank == False or A_array[(n - 1) * n + n - 1] == 0:
-    return False
-  else:
-    return True
-
-
-# check whether A is full rank in a greedy fashion
-# @param        A_array             the coefficient matrix of the system of linear equations
-def is_full_rank_greedy(A_array):
-  N = int(math.sqrt(len(A_array)))
-  N_local = N - 2
-  full_rank = True
-  # gaussian elimination
-  for k in range(N - 1):
-    print k
-    # update the denominator
-    if N_local > 0 and k == N_local:
-      A_array[N_local * N + N_local] = coefficient_Dic[(N_local, N_local)]
-    # find the max row and switch it with row k
-    max_row = k
-    if k >= N_local:
-
-      max_val = abs(A_array[k * N + k])
-      # find the max row
-      for i in range(k + 1, N):
-        if abs(A_array[i * N + k]) > max_val:
-          max_val = abs(A_array[i * N + k])
-          max_row = i
-      # check rank
-      print max_val
-      if max_val == 0:
-        full_rank = False
-        break
-      # record max row
-      max_row_Dic[k] = max_row
-    else:
-      max_row = max_row_Dic[k]
-
-    # switch it with row k
-    if k != max_row:
-      for j in range(k, N):
-        A_array[max_row * N + j], A_array[k * N + j] = A_array[k * N + j], A_array[max_row * N + j]
-
-    # division
-    denominator = 0
-    if k < N_local:
-      denominator = denominator_Dic[k]
-    else:
-      denominator_Dic[k] = A_array[k * N + k]
-      denominator = denominator_Dic[k]
-    for j in range(k, N):
-      if j > N_local or (j == k and j == N_local):
-        A_array[k * N + j] /= denominator
-
-    # subtraction
-    for i in range(k + 1, N):
-      multiplier = 0
-      if i <= N_local:
-        multiplier = multiplier_Dic[(i, k)]
-      else:
-        multiplier_Dic[(i, k)] = A_array[i * N + k]
-        multiplier = multiplier_Dic[(i, k)]
-      for j in range(k, N):
-        if i > N_local or j > N_local:
-          if j <= N_local and k < N_local:
-            A_array[i * N + j] -= coefficient_Dic[(k, j)] * multiplier
-          else:
-            A_array[i * N + j] -= A_array[k * N + j] * multiplier
-
-    for i in range(N):
-      val_L = []
-      for j in range(N):
-        val_L.append(A_array[i * N + j])
-      print val_L
-    print
-  
-  # return
-  print [full_rank, A_array[(N - 1) * N + N - 1]]
-  if full_rank == False or A_array[(N - 1) * N + N - 1] == 0:
-    return False
-  else:
-    # update coefficient_Dic
-    for i in range(N):
-      for j in range(N):
-        if i > N_local or j > N_local or (i == j and i == N_local):
-          coefficient_Dic[(i, j)] = A_array[i * N + j]
-    return True
-
-
-# get B_array, the value vector of the system of linear equations
-# @param        A_array             the coefficient matrix of the system of linear equations
+# get B_L, the value vector of the system of linear equations
+# @param        A_LL             the coefficient matrix of the system of linear equations
 # @param        X_L                 the set of potential causes
 # @param        e                   an effect
-def get_B_array(X_L, e):
+def get_B_L(X_L, e):
   n = len(X_L)
-  B_array = []
+  B_L = []
   for i in range(n):
     c_L = X_L[i]
     T_e_c_L = get_T_e_c_L(c_L)
@@ -354,8 +221,8 @@ def get_B_array(X_L, e):
     nominator = len_T_e * len(T_e_c_L)
     denominator = N_e_c * (len_T_e - len(T_e_c_L))
     f_e_c = nominator / denominator
-    B_array.append(f_e_c * (E_e_c - E_e))
-  return B_array
+    B_L.append(f_e_c * (E_e_c - E_e))
+  return B_L
 
 
 # is c in Xt? i.e. is c a potential cause of e being measured at time t
@@ -376,69 +243,29 @@ def is_in_Xt(c_L, time):
     return False
 
 
-# solve system of linear equations
-# @param        A_array             the coefficient matrix of the system of linear equations
-# @param        A_array             the value vector of the system of linear equations
-def solve_system_of_linear_equations(A_array, B_array):
-  n = int(math.sqrt(len(A_array)))
-  # gaussian elimination
-  for k in range(n - 1):
-    n = int(math.sqrt(len(A_array)))
-    # find the max row and switch it with row k
-    max_val = abs(A_array[k * n + k])
-    max_row = k
-    # find the max row
-    for i in range(k + 1, n):
-      if abs(A_array[i * n + k]) > max_val:
-        max_val = abs(A_array[i * n + k])
-        max_row = i
-    # switch it with row k
-    if k != max_row:
-      for j in range(k, n):
-        A_array[max_row * n + j], A_array[k * n + j] = A_array[k * n + j], A_array[max_row * n + j]
-      B_array[max_row], B_array[k] = B_array[k], B_array[max_row]
-    # division
-    denominator = A_array[k * n + k]
-    for j in range(k, n):
-      A_array[k * n + j] /= denominator
-    B_array[k] /= denominator
-    # subtraction
-    for i in range(k + 1, n):
-      multiplier = A_array[i * n + k]
-      for j in range(k, n):
-        A_array[i * n + j] -= A_array[k * n + j] * multiplier
-      B_array[i] -= B_array[k] * multiplier
-
-  # get solution
-  B_array[n - 1] /= A_array[(n - 1) * n + n - 1]
-  for i in reversed(range(n - 1)):
-    for j in range(i + 1, n):
-      B_array[i] -= A_array[i * n + j] * B_array[j]
-  return B_array
-
-# get result_file
+# get alpha_file
 # @param        X_L                 the set of potential causes
 # @param        e                   an effect
-# @param        B_array             the value vector of the system of linear equations
-# @param        result_file         file containing relationships and their causal significance
-def get_result_file(X_L, e, B_array, result_file):
+# @param        B_L             the value vector of the system of linear equations
+# @param        alpha_file         file containing relationships and their causal significance
+def get_alpha_file(X_L, e, B_L, alpha_file):
   for i in range(len(X_L)):
     c, r, s = X_L[i]
-    with open(result_file, 'a') as f:
+    with open(alpha_file, 'a') as f:
       spamwriter = csv.writer(f, delimiter = ',')
-      spamwriter.writerow([c, e, r, s, B_array[i]])
+      spamwriter.writerow([c, e, r, s, B_L[i]])
 
 
 # get a linearly independent subset of X
 # @param        X_L                 the set of potential causes
 # @param        e                   an effect
 def get_X_LIS_L(X_L, e):
-  print "get_X_LIS_L"
   n = len(X_L)
   X_LIS_L = []
   c_L_abs_dif_L = []
   for c_L in X_L:
     c_L_abs_dif_L.append([c_L, abs(get_E_e_c(e, c_L) - get_E_e(e))])
+
   # bubble sort 
   for i in reversed(range(n - 1)):
     for j in range(i + 1):
@@ -449,9 +276,9 @@ def get_X_LIS_L(X_L, e):
   for [c_L, abs_dif] in c_L_abs_dif_L:
     X_LIS_L.append(c_L)
     if len(X_LIS_L) > 1:
-      A_LIS_array = get_A_array(X_LIS_L)
-      rank = np.linalg.matrix_rank(A_LIS_array)
-      if rank < len(A_LIS_array):
+      A_LIS_LL = get_A_LL(X_LIS_L)
+      rank = np.linalg.matrix_rank(A_LIS_LL)
+      if rank < len(A_LIS_LL):
         X_LIS_L.remove(c_L)
   return X_LIS_L
 
@@ -477,12 +304,6 @@ def get_global_variables(disc_data_file, cont_data_file, header, transpose):
   disc_var_time_val_LLL = get_var_time_val_LLL(disc_data_file, header, transpose, "discrete")
   cont_var_time_val_LLL = get_var_time_val_LLL(cont_data_file, header, transpose, "continuous")
 
-  # initialize global variables
-  # relations_Dic = {}
-  # disc_val_Dic = {}
-  # time_cont_val_L_Dic = {}
-  # cont_val_L_Dic = {}
-
   # get disc_val_Dic
   for [var, time_val_LL] in disc_var_time_val_LLL:
     for [time, val] in time_val_LL:
@@ -505,7 +326,7 @@ def get_global_variables(disc_data_file, cont_data_file, header, transpose):
       if not var in cont_val_L_Dic:
         cont_val_L_Dic[var] = {}
       cont_val_L_Dic[var][time] = val
- 
+
   # get alphabet_disc
   for time in disc_val_Dic:
     for var in disc_val_Dic[time]:
@@ -628,6 +449,7 @@ def get_E_e_c(e, c_L):
   else:
     return None
 
+
 # get E[e]
 # @param        e                   an effect
 def get_E_e(e):
@@ -658,11 +480,11 @@ if __name__=="__main__":
   header = sys.argv[3]
   transpose = sys.argv[4]
   rel_type = sys.argv[5]
-  result_file = sys.argv[6]
+  alpha_file = sys.argv[6]
   win_L = [[1, 1]]
   # make directory
-  # if not os.path.exists(result_file):
-    # os.makedirs(result_file)
+  # if not os.path.exists(alpha_file):
+    # os.makedirs(alpha_file)
 
   # get global variables
   get_global_variables(disc_data_file, cont_data_file, header, transpose)
@@ -673,7 +495,7 @@ if __name__=="__main__":
     test_hypotheses(hyp, rel_type)
 
   # calculate alpha for each relationship
-  get_all_alpha(result_file)
+  get_all_alpha(alpha_file)
 
 
 
