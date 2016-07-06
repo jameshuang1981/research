a272f55d46c7d9ea08513abbda89c25e2a13e4c0
yuxiao huang <yuxiaohuang@yuxiaos-Air.home>
2016-07-06 11:36:30 -0400

    update comment

--- code/FLAIRS_15/python/alpha.py
+++ code/FLAIRS_15/python/alpha.py
@@ -66,11 +66,11 @@ disc_val_L_Dic = {}
 
 # key: continuous_valued var
 # val: list of var's continuous value at each time
-cont_val_L_Dic = {}
+cont_val_Dic = {}
 
 # key: continuous_valued var
 # val: list of time and var's continuous value at that time, i.e. [[time, val]]
-time_cont_val_L_Dic = {}
+time_cont_val_Dic = {}
 
 # list of discrete or discretized vars in the time series
 alphabet_disc = []
@@ -100,8 +100,8 @@ def get_all_alpha(alpha_file):
 
 
 # calculate alpha for one relationship
-# @param        e                  a continuous-valued variable
-# @param        alpha_file         file containing relationships and their causal significance
+# @param        e                  a continuous_valued variable
+# @param        alpha_file         file containing relationships and their causal significance (i.e. alpha)
 def get_alpha(e, alpha_file):
   # the key is building a system of linear equations, A * alpha = B
   # where A is a matrix (denoted by A_LL) and B a column vector (denoted by B_L)
@@ -287,106 +287,128 @@ def get_alpha_file(X_L, e, B_L, alpha_file):
 
 
 # get a linearly independent subset of X
+# please see details of the method in the FLAIRS paper mentioned in the beginning
 # @param        X_L                the set of potential causes
 # @param        e                  an effect
 def get_X_LIS_L(X_L, e):
   n = len(X_L)
   X_LIS_L = []
-  c_L_abs_dif_L = []
+  c_T_abs_dif_L = []
   for c_T in X_L:
-    c_L_abs_dif_L.append([c_T, abs(get_E_e_c(e, c_T) - get_E_e(e))])
+    c_T_abs_dif_L.append([c_T, abs(get_E_e_c(e, c_T) - get_E_e(e))])
 
   # bubble sort 
   for i in reversed(range(n - 1)):
     for j in range(i + 1):
-      if c_L_abs_dif_L[j][1] < c_L_abs_dif_L[j + 1][1]:
-        c_L_abs_dif_L[j], c_L_abs_dif_L[j + 1] = c_L_abs_dif_L[j + 1], c_L_abs_dif_L[j]
+      if c_T_abs_dif_L[j][1] < c_T_abs_dif_L[j + 1][1]:
+        c_T_abs_dif_L[j], c_T_abs_dif_L[j + 1] = c_T_abs_dif_L[j + 1], c_T_abs_dif_L[j]
 
   # greedy search
-  for [c_T, abs_dif] in c_L_abs_dif_L:
+  for [c_T, abs_dif] in c_T_abs_dif_L:
     X_LIS_L.append(c_T)
     if len(X_LIS_L) > 1:
+      # get A_LIS_LL from X_LIS_L, which is the same as getting A_LL from X_L (in function get_alpha)
       A_LIS_LL = get_A_LL(X_LIS_L)
+      # get the rank of A_LIS_LL 
       rank = np.linalg.matrix_rank(A_LIS_LL)
+      # if A_LIS_LL is not full rank (i.e. the subsystem of linear equations has no unique solutions)
       if rank < len(A_LIS_LL):
         X_LIS_L.remove(c_T)
   return X_LIS_L
 
 
-# get global_variables: disc_val_L_Dic, time_cont_val_L_Dic, alphabet_disc, alphabet_cont
-# @param        disc_data_file     time series data of form
-#                                  var1_t1, var1_t2, ..., var1_tn
-#                                  var2_t1, var2_t2, ..., varn_tn
-# @param        cont_data_file     time series data of form
-#                                  var1_t1, var1_t2, ..., var1_tn
-#                                  var2_t1, var2_t2, ..., varn_tn
-# @param        header             True,  if there is a header
+# get global_variables: disc_val_L_Dic, time_cont_val_Dic, cont_val_Dic, alphabet_disc, alphabet_cont
+# @param        disc_data_file     discrete data file, which includes variables that can be the causes, the data are of the following form
+#                                  var1 (i.e. header), var1_t1 (i.e. val), ..., var1_tn (i.e. val)
+#                                  , ...,
+#                                  varn (i.e. header), varn_t1 (i.e. val), ..., varn_tn (i.e. val)
+# @param        cont_data_file     continuous data file, which includes variables that can be the effects, the data are of the following form
+#                                  var1 (i.e. header), var1_t1 (i.e. val), ..., var1_tn (i.e. val)
+#                                  , ...,
+#                                  varn (i.e. header), varn_t1 (i.e. val), ..., varn_tn (i.e. val)
+# @param        header             tells us whether the data files have a header
+#                                  True,  if there is a header
 #                                  False, otherwise
 # @param        transpose          tells us whether the data need to be transposed
 #                                  False, when the data are of the above form
 #                                  True,  when the data are of the following form
-#                                  var1_t1, var2_t1, ..., varn_t1
-#                                  var1_t2, var2_t2, ..., varn_tn
+#                                  var1 (i.e. header), ..., varn (i.e. header)
+#                                  var1_t1 (i.e. val), ..., varn_t1 (i.e. val)
+#                                  , ...,
+#                                  var1_tn (i.e. val), ..., varn_tn (i.e. val)
 def get_global_variables(disc_data_file, cont_data_file, header, transpose):
+  # get data from files
   disc_var_time_val_LLL = get_var_time_val_LLL(disc_data_file, header, transpose, "discrete")
   cont_var_time_val_LLL = get_var_time_val_LLL(cont_data_file, header, transpose, "continuous")
 
   # get disc_val_L_Dic
+  # the key is time and the value is a list of discrete or discretized variables being true and the time
+  # e.g. at time 1, "glucose" is "High" and "insulin" is "Low", then disc_val_L_Dic[1] = ["glucose_High", "insulin_Low"]
   for [var, time_val_LL] in disc_var_time_val_LLL:
     for [time, val] in time_val_LL:
-      # discrete val is a string, e.g. "High", "Normal", or "Low"
+      # discrete val is a string representing a category, e.g. "High", "Normal", or "Low"
       # check empty string
       if val:
         if not time in disc_val_L_Dic:
           disc_val_L_Dic[time] = []
+        # concatenate the val to the name, e.g. var "glucose" with value "High" will be "glucose_High"
         disc_val_L_Dic[time].append(var + "_" + val)
 
-  # get time_cont_val_L_Dic
+  # get time_cont_val_Dic
+  # the key is var (i.e. name) and the value is a list of [time, val]
+  # e.g. "glucose" is 100 at time 1 and 110 at time 2, then cont_var_time_val_LLL["glucose"] = [[1, 100], [2, 110]]
   for [var, time_val_LL] in cont_var_time_val_LLL:
     for [time, val] in time_val_LL:
+      # continuous val is a string representing a number, e.g. "100"
       # check string that is not a number
       if is_number(val):
         val = float(val)
-        if not var in time_cont_val_L_Dic:
-          time_cont_val_L_Dic[var] = []
-        time_cont_val_L_Dic[var].append([time, val])
+        if not var in time_cont_val_Dic:
+          time_cont_val_Dic[var] = []
+        time_cont_val_Dic[var].append([time, val])
 
-  # get cont_val_L_Dic
-  # for cases where variables are not measured at every timepoint
+  # get cont_val_Dic
+  # the first key is var (i.e. name) and the second key is time
+  # the value is val
+  # e.g. "glucose" is 100 at time 1, then cont_val_Dic["glucose"][1] = 100
+  # this dictionary is used for cases where variables are not measured at every timepoint
   for [var, time_val_LL] in cont_var_time_val_LLL:
     for [time, val] in time_val_LL:
       # check string that is not a number
       if is_number(val):
         val = float(val)
-        if not var in cont_val_L_Dic:
-          cont_val_L_Dic[var] = {}
-        cont_val_L_Dic[var][time] = val
+        if not var in cont_val_Dic:
+          cont_val_Dic[var] = {}
+        cont_val_Dic[var][time] = val
 
   # get alphabet_disc
+  # e.g., ["glucose_High", "insulin_low"]
   for time in disc_val_L_Dic:
     for var in disc_val_L_Dic[time]:
       if not var in alphabet_disc:
         alphabet_disc.append(var)
 
   # get alphabet_cont
-  for var in time_cont_val_L_Dic:
+  # e.g., ["glucose', 'insulin"]
+  for var in time_cont_val_Dic:
     alphabet_cont.append(var)
 
 
 # get [var, [time, val]]
-# @param        time_series_file   time series data of form
-#                                  var1_t1, ..., var1_tn
+# @param        data_file          the data are of the following form
+#                                  var1 (i.e. header), var1_t1 (i.e. val), ..., var1_tn (i.e. val)
 #                                  , ...,
-#                                  varn_t1, ..., varn_tn
-# @param        header             tells us whether the data file has a header
+#                                  varn (i.e. header), varn_t1 (i.e. val), ..., varn_tn (i.e. val)
+# @param        header             tells us whether the data files have a header
 #                                  True,  if there is a header
 #                                  False, otherwise
 # @param        transpose          tells us whether the data need to be transposed
-#                                  False, when the data are of the above form
+#                                  False, when the data are of the above form, thus do not need to be transposed
 #                                  True,  when the data are of the following form, thus need to be transposed
-#                                  var1_t1, ..., varn_t1
+#                                  var1 (i.e. header), ..., varn (i.e. header)
+#                                  var1_t1 (i.e. val), ..., varn_t1 (i.e. val)
 #                                  , ...,
-#                                  var1_tn, ..., varn_tn
+#                                  var1_tn (i.e. val), ..., varn_tn (i.e. val)
 # @param        data_type          "discrete",   if discrete data
 #                                  "continuous", if continuous_valued data
 def get_var_time_val_LLL(data_file, header, transpose, data_type):
@@ -435,10 +457,10 @@ def is_number(val):
     return False
 
 
-# generate hypotheses for an effect
-# a hypothesis is of form: [cause, effect, window_start, window_end], or [c, e, r, s] for simplicity
+# generate hypotheses, where a hypothesis is of the following form
+# [cause, effect, window_start, window_end], or [c, e, r, s] for simplicity
 # @param        c_L                a list of discrete or discretized vars
-# @param        e_L                [e]
+# @param        e_L                a list of continuous_valued vars
 # @param        r                  the start of a time window, i.e. r in window [r, s]
 # @param        s                  the end of a time window, i.e. s in window [r, s]
 def generate_hypotheses(c_L, e_L, r, s):
@@ -450,17 +472,18 @@ def generate_hypotheses(c_L, e_L, r, s):
 
 
 # test hypotheses
-# for hypothesis [c, e, r, s], test whether c is a potential cause of e related to time window [r, s] and get relations_Dic
+# for hypothesis [c, e, r, s], 1) test whether c is a potential cause of e related to time window [r, s], and 2) get relations_Dic
 # @param        hypotheses         a hypothesis is of form: [c, e, r, s]
-# @param        rel_type           type of hypotheses we want to test
-#                                  "not_equal" for hypotheses s.t. E_e_c != E_e
-#                                  "positive"  for hypotheses s.t. E_e_c > E_e
-#                                  "negative"  for hypotheses s.t. E_e_c < E_e
+# @param        rel_type           tells us the type of hypotheses we want to test
+#                                  "not_equal" for hypotheses s.t. E[e | c] != E[e]
+#                                  "positive"  for hypotheses s.t. E[e | c] > E[e]
+#                                  "negative"  for hypotheses s.t. E[e_| c] < E[e]
 #                                  "all"       for all hypotheses
 def test_hypotheses(hypotheses, rel_type):
   for [c, e, r, s] in hypotheses:
     E_e_c = get_E_e_c(e, (c, r, s))
     E_e = get_E_e(e)
+    # check whether E[e | c] and E[e] are valid
     if E_e_c != None and E_e != None:
       if rel_type == "not_equal":
         if E_e_c != E_e:
@@ -484,8 +507,9 @@ def get_E_e_c(e, c_T):
     if not (e, c_T) in E_e_c_Dic:
       val_L = []
       for time in T_e_c_L:
-        if time in cont_val_L_Dic[e]:
-          val_L.append(cont_val_L_Dic[e][time])
+        if time in cont_val_Dic[e]:
+          val_L.append(cont_val_Dic[e][time])
+      # check whether val_L is empty
       if not val_L:
         return None
       E_e_c = np.mean(val_L)
@@ -500,8 +524,9 @@ def get_E_e_c(e, c_T):
 def get_E_e(e):
   if not e in E_e_Dic:
     val_L = []
-    for time in cont_val_L_Dic[e]:
-      val_L.append(cont_val_L_Dic[e][time])
+    for time in cont_val_Dic[e]:
+      val_L.append(cont_val_Dic[e][time])
+    # check whether val_L is empty
     if not val_L:
       return None
     E_e = np.mean(val_L)
@@ -520,15 +545,17 @@ def add_relationship(c, e, r, s):
   relations_Dic[e].append((c, r, s))
 
 
-# get significant relationships based on significance test
-# @param        sig_rel_file       significant relationship file
-# @param        p_val cutoff       p value cutoff
-# @param        family_type        "one", significance test based on one family
+# get significant relationships based on significance test (z_test)
+# @param        sig_rel_file       file containing significant relationships 
+# @param        family_type        tells us whether to test the relationships for all effects (all families) at a time, or the relationships for each effect (one family) at a time
+# @param        p_val cutoff       p value cutoff, which is used to decide significant relationships 
+#                                  "one", significance test based on one family
 #                                  "all", significance test based on all families
-# @param        tail_type          "positive", if p_val < p_val_cutoff and z_val > 0
+# @param        tail_type          tells us the type of z_test we want to do
+#                                  "positive", if p_val < p_val_cutoff and z_val > 0
 #                                  "negative", if p_val < p_val_cutoff and z_val < 0
 #                                  "both",     if p_val < p_val_cutoff
-def significance_test(sig_rel_file, p_val_cutoff, family_type, tail_type):
+def significance_test(sig_rel_file, family_type, p_val_cutoff, tail_type):
   # output heading
   with open(sig_rel_file, 'wb') as f:
     spamwriter = csv.writer(f, delimiter = ',')
@@ -544,12 +571,13 @@ def significance_test(sig_rel_file, p_val_cutoff, family_type, tail_type):
 
 
 # get significant relationship file
-# @param        sig_rel_file       significant relationship file
-# @param        p_val cutoff       p value cutoff
-# @param        tail_type          "positive", if p_val < p_val_cutoff and z_val > 0
+# @param        sig_rel_file       file containing significant relationships 
+# @param        p_val cutoff       p value cutoff, which is used to decide significant relationships 
+# @param        tail_type          tells us the type of z_test we want to do
+#                                  "positive", if p_val < p_val_cutoff and z_val > 0
 #                                  "negative", if p_val < p_val_cutoff and z_val < 0
 #                                  "both",     if p_val < p_val_cutoff
-# @param        e_L                [e], a list of effects
+# @param        e_L                a list of effects
 def get_sig_rel_file(sig_rel_file, p_val_cutoff, tail_type, e_L):
   # get alpha_LL
   alpha_LL = []
@@ -591,6 +619,7 @@ def get_sig_rel_file(sig_rel_file, p_val_cutoff, tail_type, e_L):
 # main function
 if __name__=="__main__":
   # get parameters
+  # please see details of the parameters in the readme file
   disc_data_file = sys.argv[1]
   cont_data_file = sys.argv[2]
   header = sys.argv[3]
@@ -598,10 +627,11 @@ if __name__=="__main__":
   rel_type = sys.argv[5]
   alpha_file = sys.argv[6]
   sig_rel_file = sys.argv[7]
-  p_val_cutoff = float(sys.argv[8])
-  family_type = sys.argv[9]
+  family_type = sys.argv[8]
+  p_val_cutoff = float(sys.argv[9])
   tail_type = sys.argv[10]
   lag_L = sys.argv[11:]
+  # get win_L from lag_L
   win_L = []
   for i in range(0, len(lag_L), 2):
     win = [int(lag_L[i]), int(lag_L[i + 1])]
@@ -623,8 +653,8 @@ if __name__=="__main__":
     hyp = generate_hypotheses(alphabet_disc, alphabet_cont, r, s)
     test_hypotheses(hyp, rel_type)
 
-  # calculate alpha for each relationship
+  # calculate alpha for all relationships
   get_all_alpha(alpha_file)
 
-  # get significant relationships where p_val(alpha) < p_val_cutoff
-  significance_test(sig_rel_file, p_val_cutoff, family_type, tail_type)
+  # get significant relationships based on family_type,  p_val_cutoff, and tail_type
+  significance_test(sig_rel_file, family_type, p_val_cutoff, tail_type)
