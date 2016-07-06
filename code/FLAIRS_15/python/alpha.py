d437d85b0f0326ba07d1fadf113925df93358d52
yuxiao huang <yuxiaohuang@yuxiaos-Air.home>
2016-07-05 23:09:11 -0400

    update comment

--- code/FLAIRS_15/python/alpha.py
+++ code/FLAIRS_15/python/alpha.py
@@ -137,20 +137,19 @@ def get_alpha(e, alpha_file):
 
 
 # get A_LL, the coefficient matrix of the system of linear equations
-# please see details of A_LL in the FLAIRS paper mentioned in the beginning
-# @param        X_L                 the set of potential causes
+# @param        X_L                the set of potential causes
 def get_A_LL(X_L):
   n = len(X_L)
   A_LL = []
   for i in range(n):
     row_L = []
     for j in range(n):
-      c_L = X_L[i]
-      x_L = X_L[j]
-      N_e_c_x = get_N_e_c_x(c_L, x_L)
-      N_e_x = get_N_e_c(x_L)
-      T_e_c_L = get_T_e_c_L(c_L)
-      N_e_c = get_N_e_c(c_L)
+      c_T = X_L[i]
+      x_T = X_L[j]
+      N_e_c_x = get_N_e_c_x(c_T, x_T)
+      N_e_x = get_N_e_c(x_T)
+      T_e_c_L = get_T_e_c_L(c_T)
+      N_e_c = get_N_e_c(c_T)
       len_T_e = len(disc_val_L_Dic)
       nominator = N_e_c_x * len_T_e - N_e_x * len(T_e_c_L)
       denominator = N_e_c * (len_T_e - len(T_e_c_L))
@@ -160,29 +159,27 @@ def get_A_LL(X_L):
   return A_LL
 
 
-# get *N_e_c_x_Dic*
-# @param        c_L                (c, r, s), a list including potential cause c, start and end of time window, r and s
-# @param        x_L                (list x r s), a list including potential cause x, start and end of time window, r and s
-def get_N_e_c_x(c_L, x_L):
-  c, cr, cs = c_L
-  x, xr, xs = x_L
-  if not ((c, cr, cs), (x, xr, xs)) in N_e_c_x_Dic:
-    T_e_xt_LL = get_T_e_ct_LL(x_L)
+# get N_e_c_x_Dic
+# @param        c_T                (c, r, s), a tuple including potential cause c, start and end of time window, r and s
+# @param        x_T                (x, r, s), a tuple including potential cause x, start and end of time window, r and s
+def get_N_e_c_x(c_T, x_T):
+  if not (c_T, x_T) in N_e_c_x_Dic:
+    T_e_xt_LL = get_T_e_ct_LL(x_T)
     N_e_c_x = 0
     for T_e_xt_L in T_e_xt_LL:
       for time in T_e_xt_L:
-        if is_in_Xt(c_L, time):
+        if is_in_Xt(c_T, time):
           N_e_c_x += 1
-    N_e_c_x_Dic[((c, cr, cs), (x, xr, xs))] = N_e_c_x
-  return N_e_c_x_Dic[((c, cr, cs), (x, xr, xs))]
+    N_e_c_x_Dic[(c_T, x_T)] = N_e_c_x
+  return N_e_c_x_Dic[(c_T, x_T)]
 
 
-# get *T_e_ct_LL_Dic*
-# @param        c_L                (c, r, s), a list including potential cause c, start and end of time window, r and s
-def get_T_e_ct_LL(c_L):
-  c, r, s = c_L
-  if not (c, r, s) in T_e_ct_LL_Dic:
-    T_c_L = get_T_c_L(c)
+# get T_e_ct_LL_Dic
+# @param       c_T                 (c, r, s), a tuple including potential cause c, start and end of time window, r and s
+def get_T_e_ct_LL(c_T):
+  (c, r, s) = c_T
+  if not c_T in T_e_ct_LL_Dic:
+    T_c_L = get_T_c_L_Dic(c)
     T_e_ct_LL = []
     for tc in T_c_L:
       T_e_ct_L = []
@@ -191,13 +188,13 @@ def get_T_e_ct_LL(c_L):
           T_e_ct_L.append(time)
       if T_e_ct_L:
         T_e_ct_LL.append(T_e_ct_L)
-    T_e_ct_LL_Dic[(c, r, s)] = T_e_ct_LL
-  return T_e_ct_LL_Dic[(c, r, s)]
+    T_e_ct_LL_Dic[c_T] = T_e_ct_LL
+  return T_e_ct_LL_Dic[c_T]
 
 
-# get *T_c_L_Dic*
-# @param        c                   a potential cause 
-def get_T_c_L(c):
+# get T_c_L_Dic
+# @param        c                  a potential cause 
+def get_T_c_L_Dic(c):
   if not c in T_c_L_Dic:
     T_c_L = []
     for time in disc_val_L_Dic:
@@ -207,46 +204,44 @@ def get_T_c_L(c):
   return T_c_L_Dic[c]
 
 
-# get *T_e_c_L_Dic*
-# @param        c_L                (c, r, s), a list including potential cause c, start and end of time window, r and s
-def get_T_e_c_L(c_L):
-  c, r, s = c_L
-  if not (c, r, s) in T_e_c_L_Dic:
-    T_e_ct_LL = get_T_e_ct_LL(c_L)
+# get T_e_c_L_Dic
+# @param       c_T                 (c, r, s), a tuple including potential cause c, start and end of time window, r and s
+def get_T_e_c_L(c_T):
+  if not c_T in T_e_c_L_Dic:
+    T_e_ct_LL = get_T_e_ct_LL(c_T)
     T_e_c_L = []
     for T_e_ct_L in T_e_ct_LL:
       for time in T_e_ct_L:
         T_e_c_L.append(time)
     T_e_c_L = list(set(T_e_c_L))
-    T_e_c_L_Dic[(c, r, s)] = T_e_c_L
-  return T_e_c_L_Dic[(c, r, s)]
+    T_e_c_L_Dic[c_T] = T_e_c_L
+  return T_e_c_L_Dic[c_T]
 
 
-# get *N_e_c_Dic*
-# @param        c_L                (c, r, s), a list including potential cause c, start and end of time window, r and s
-def get_N_e_c(c_L):
-  c, r, s = c_L
-  if not (c, r, s) in N_e_c_Dic:
-    T_e_ct_LL = get_T_e_ct_LL(c_L)
+# get N_e_c_Dic
+# @param       c_T                 (c, r, s), a tuple including potential cause c, start and end of time window, r and s
+def get_N_e_c(c_T):
+  if not c_T in N_e_c_Dic:
+    T_e_ct_LL = get_T_e_ct_LL(c_T)
     N_e_c = 0
     for T_e_ct_L in T_e_ct_LL:
       N_e_c += len(T_e_ct_L)
-    N_e_c_Dic[(c, r, s)] = N_e_c
-  return N_e_c_Dic[(c, r, s)]
+    N_e_c_Dic[c_T] = N_e_c
+  return N_e_c_Dic[c_T]
 
 
-# get B_L, the value vector of the system of linear equations
-# @param        A_LL             the coefficient matrix of the system of linear equations
-# @param        X_L                 the set of potential causes
-# @param        e                   an effect
+# get B_L, the column vector of the system of linear equations
+# @param        A_LL               the coefficient matrix of the system of linear equations
+# @param        X_L                the set of potential causes
+# @param        e                  an effect
 def get_B_L(X_L, e):
   n = len(X_L)
   B_L = []
   for i in range(n):
-    c_L = X_L[i]
-    T_e_c_L = get_T_e_c_L(c_L)
-    N_e_c = get_N_e_c(c_L)
-    E_e_c = get_E_e_c(e, c_L)
+    c_T = X_L[i]
+    T_e_c_L = get_T_e_c_L(c_T)
+    N_e_c = get_N_e_c(c_T)
+    E_e_c = get_E_e_c(e, c_T)
     E_e = get_E_e(e)
     len_T_e = len(disc_val_L_Dic)
     nominator = len_T_e * len(T_e_c_L)
@@ -257,32 +252,31 @@ def get_B_L(X_L, e):
 
 
 # is c in Xt? i.e. is c a potential cause of e being measured at time t
-# @param        c_L                (c, r, s), a list including potential cause c, start and end of time window, r and s
+# @param        c_T                (c, r, s), a tuple including potential cause c, start and end of time window, r and s
 # @param        time               the time where e is measured
-def is_in_Xt(c_L, time):
-  c, r, s = c_L
-  if not (c, r, s) in is_in_Xt_L_Dic:
-    T_e_c_L = get_T_e_c_L(c_L)
+def is_in_Xt(c_T, time):
+  if not c_T in is_in_Xt_L_Dic:
+    T_e_c_L = get_T_e_c_L(c_T)
     for te in T_e_c_L:
       if not te in Xt_L_Dic:
         Xt_L_Dic[te] = []
-      Xt_L_Dic[te].append(c_L)
-    is_in_Xt_L_Dic[(c, r, s)] = True
-  if time in Xt_L_Dic and c_L in Xt_L_Dic[time]:
+      Xt_L_Dic[te].append(c_T)
+    is_in_Xt_L_Dic[c_T] = True
+  if time in Xt_L_Dic and c_T in Xt_L_Dic[time]:
     return True
   else:
     return False
 
 
 # get alpha_file
-# @param        X_L                 the set of potential causes
-# @param        e                   an effect
-# @param        B_L             the value vector of the system of linear equations
+# @param        X_L                the set of potential causes
+# @param        e                  an effect
+# @param        B_L                the column vector of the system of linear equations
 # @param        alpha_file         file containing relationships and their causal significance
 def get_alpha_file(X_L, e, B_L, alpha_file):
   with open(alpha_file, 'a') as f:
     for i in range(len(X_L)):
-      c, r, s = X_L[i]
+      (c, r, s) = X_L[i]
       alpha = B_L[i]
       spamwriter = csv.writer(f, delimiter = ',')
       spamwriter.writerow([c, e, r, s, alpha])
@@ -293,14 +287,14 @@ def get_alpha_file(X_L, e, B_L, alpha_file):
 
 
 # get a linearly independent subset of X
-# @param        X_L                 the set of potential causes
-# @param        e                   an effect
+# @param        X_L                the set of potential causes
+# @param        e                  an effect
 def get_X_LIS_L(X_L, e):
   n = len(X_L)
   X_LIS_L = []
   c_L_abs_dif_L = []
-  for c_L in X_L:
-    c_L_abs_dif_L.append([c_L, abs(get_E_e_c(e, c_L) - get_E_e(e))])
+  for c_T in X_L:
+    c_L_abs_dif_L.append([c_T, abs(get_E_e_c(e, c_T) - get_E_e(e))])
 
   # bubble sort 
   for i in reversed(range(n - 1)):
@@ -309,30 +303,30 @@ def get_X_LIS_L(X_L, e):
         c_L_abs_dif_L[j], c_L_abs_dif_L[j + 1] = c_L_abs_dif_L[j + 1], c_L_abs_dif_L[j]
 
   # greedy search
-  for [c_L, abs_dif] in c_L_abs_dif_L:
-    X_LIS_L.append(c_L)
+  for [c_T, abs_dif] in c_L_abs_dif_L:
+    X_LIS_L.append(c_T)
     if len(X_LIS_L) > 1:
       A_LIS_LL = get_A_LL(X_LIS_L)
       rank = np.linalg.matrix_rank(A_LIS_LL)
       if rank < len(A_LIS_LL):
-        X_LIS_L.remove(c_L)
+        X_LIS_L.remove(c_T)
   return X_LIS_L
 
 
 # get global_variables: disc_val_L_Dic, time_cont_val_L_Dic, alphabet_disc, alphabet_cont
-# @param        disc_data_file      time series data of form
-#                                   var1_t1, var1_t2, ..., var1_tn
-#                                   var2_t1, var2_t2, ..., varn_tn
-# @param        cont_data_file      time series data of form
-#                                   var1_t1, var1_t2, ..., var1_tn
-#                                   var2_t1, var2_t2, ..., varn_tn
-# @param        header              True,  if there is a header
-#                                   False, otherwise
-# @param        transpose           tells us whether the data need to be transposed
-#                                   False, when the data are of the above form
-#                                   True,  when the data are of the following form
-#                                   var1_t1, var2_t1, ..., varn_t1
-#                                   var1_t2, var2_t2, ..., varn_tn
+# @param        disc_data_file     time series data of form
+#                                  var1_t1, var1_t2, ..., var1_tn
+#                                  var2_t1, var2_t2, ..., varn_tn
+# @param        cont_data_file     time series data of form
+#                                  var1_t1, var1_t2, ..., var1_tn
+#                                  var2_t1, var2_t2, ..., varn_tn
+# @param        header             True,  if there is a header
+#                                  False, otherwise
+# @param        transpose          tells us whether the data need to be transposed
+#                                  False, when the data are of the above form
+#                                  True,  when the data are of the following form
+#                                  var1_t1, var2_t1, ..., varn_t1
+#                                  var1_t2, var2_t2, ..., varn_tn
 def get_global_variables(disc_data_file, cont_data_file, header, transpose):
   disc_var_time_val_LLL = get_var_time_val_LLL(disc_data_file, header, transpose, "discrete")
   cont_var_time_val_LLL = get_var_time_val_LLL(cont_data_file, header, transpose, "continuous")
@@ -380,21 +374,21 @@ def get_global_variables(disc_data_file, cont_data_file, header, transpose):
 
 
 # get [var, [time, val]]
-# @param        time_series_file    time series data of form
-#                                   var1_t1, ..., var1_tn
-#                                   , ...,
-#                                   varn_t1, ..., varn_tn
-# @param        header              tells us whether the data file has a header
-#                                   True,  if there is a header
-#                                   False, otherwise
-# @param        transpose           tells us whether the data need to be transposed
-#                                   False, when the data are of the above form
-#                                   True,  when the data are of the following form, thus need to be transposed
-#                                   var1_t1, ..., varn_t1
-#                                   , ...,
-#                                   var1_tn, ..., varn_tn
-# @param        data_type           "discrete",   if discrete data
-#                                   "continuous", if continuous_valued data
+# @param        time_series_file   time series data of form
+#                                  var1_t1, ..., var1_tn
+#                                  , ...,
+#                                  varn_t1, ..., varn_tn
+# @param        header             tells us whether the data file has a header
+#                                  True,  if there is a header
+#                                  False, otherwise
+# @param        transpose          tells us whether the data need to be transposed
+#                                  False, when the data are of the above form
+#                                  True,  when the data are of the following form, thus need to be transposed
+#                                  var1_t1, ..., varn_t1
+#                                  , ...,
+#                                  var1_tn, ..., varn_tn
+# @param        data_type          "discrete",   if discrete data
+#                                  "continuous", if continuous_valued data
 def get_var_time_val_LLL(data_file, header, transpose, data_type):
   with open(data_file, 'rb') as f:
     spamreader = list(csv.reader(f, delimiter = ','))
@@ -432,7 +426,7 @@ def get_var_time_val_LLL(data_file, header, transpose, data_type):
 
 
 # check whether string is a number
-# @param        val                 a string
+# @param        val                a string
 def is_number(val):
   try:
     float(val)
@@ -443,10 +437,10 @@ def is_number(val):
 
 # generate hypotheses for an effect
 # a hypothesis is of form: [cause, effect, window_start, window_end], or [c, e, r, s] for simplicity
-# @param        c_L                 (c, r, s), a list including potential cause c, start and end of time window, r and s
-# @param        e_L                 [e]
-# @param        r                   the start of a time window, i.e. r in window [r, s]
-# @param        s                   the end of a time window, i.e. s in window [r, s]
+# @param        c_L                a list of discrete or discretized vars
+# @param        e_L                [e]
+# @param        r                  the start of a time window, i.e. r in window [r, s]
+# @param        s                  the end of a time window, i.e. s in window [r, s]
 def generate_hypotheses(c_L, e_L, r, s):
   c_e_r_s_L = []
   for e in e_L:
@@ -457,12 +451,12 @@ def generate_hypotheses(c_L, e_L, r, s):
 
 # test hypotheses
 # for hypothesis [c, e, r, s], test whether c is a potential cause of e related to time window [r, s] and get relations_Dic
-# @param        hypotheses          a hypothesis is of form: [c, e, r, s]
-# @param        rel_type            type of hypotheses we want to test
-#                                   "not_equal" for hypotheses s.t. E_e_c != E_e
-#                                   "positive"  for hypotheses s.t. E_e_c > E_e
-#                                   "negative"  for hypotheses s.t. E_e_c < E_e
-#                                   "all"       for all hypotheses
+# @param        hypotheses         a hypothesis is of form: [c, e, r, s]
+# @param        rel_type           type of hypotheses we want to test
+#                                  "not_equal" for hypotheses s.t. E_e_c != E_e
+#                                  "positive"  for hypotheses s.t. E_e_c > E_e
+#                                  "negative"  for hypotheses s.t. E_e_c < E_e
+#                                  "all"       for all hypotheses
 def test_hypotheses(hypotheses, rel_type):
   for [c, e, r, s] in hypotheses:
     E_e_c = get_E_e_c(e, (c, r, s))
@@ -482,13 +476,12 @@ def test_hypotheses(hypotheses, rel_type):
 
 
 # get E[e|c]
-# @param        e                   an effect
-# @param        c_L                 (c, r, s), a list including potential cause c, start and end of time window, r and s
-def get_E_e_c(e, c_L):
-  c, r, s = c_L
-  T_e_c_L = get_T_e_c_L(c_L)
+# @param        e                  an effect
+# @param        c_T                (c, r, s), a tuple including potential cause c, start and end of time window, r and s
+def get_E_e_c(e, c_T):
+  T_e_c_L = get_T_e_c_L(c_T)
   if T_e_c_L:
-    if not (e, (c, r, s)) in E_e_c_Dic:
+    if not (e, c_T) in E_e_c_Dic:
       val_L = []
       for time in T_e_c_L:
         if time in cont_val_L_Dic[e]:
@@ -496,14 +489,14 @@ def get_E_e_c(e, c_L):
       if not val_L:
         return None
       E_e_c = np.mean(val_L)
-      E_e_c_Dic[(e, (c, r, s))] = E_e_c
-    return E_e_c_Dic[(e, (c, r, s))]
+      E_e_c_Dic[(e, c_T)] = E_e_c
+    return E_e_c_Dic[(e, c_T)]
   else:
     return None
 
 
 # get E[e]
-# @param        e                   an effect
+# @param        e                  an effect
 def get_E_e(e):
   if not e in E_e_Dic:
     val_L = []
@@ -517,10 +510,10 @@ def get_E_e(e):
 
 
 # add relationship [c, e, r, s] to relations_Dic
-# @param        c                   a potential cause
-# @param        e                   an effect
-# @param        r                   the start of a time window, i.e. r in window [r, s]
-# @param        s                   the end of a time window, i.e. s in window [r, s]
+# @param        c                  a potential cause
+# @param        e                  an effect
+# @param        r                  the start of a time window, i.e. r in window [r, s]
+# @param        s                  the end of a time window, i.e. s in window [r, s]
 def add_relationship(c, e, r, s):
   if not e in relations_Dic:
     relations_Dic[e] = []
@@ -528,13 +521,13 @@ def add_relationship(c, e, r, s):
 
 
 # get significant relationships based on significance test
-# @param        sig_rel_file        significant relationship file
-# @param        p_val cutoff        p value cutoff
-# @param        family_type         "one", significance test based on one family
-#                                   "all", significance test based on all families
-# @param        tail_type           "positive", if p_val < p_val_cutoff and z_val > 0
-#                                   "negative", if p_val < p_val_cutoff and z_val < 0
-#                                   "both",     if p_val < p_val_cutoff
+# @param        sig_rel_file       significant relationship file
+# @param        p_val cutoff       p value cutoff
+# @param        family_type        "one", significance test based on one family
+#                                  "all", significance test based on all families
+# @param        tail_type          "positive", if p_val < p_val_cutoff and z_val > 0
+#                                  "negative", if p_val < p_val_cutoff and z_val < 0
+#                                  "both",     if p_val < p_val_cutoff
 def significance_test(sig_rel_file, p_val_cutoff, family_type, tail_type):
   # output heading
   with open(sig_rel_file, 'wb') as f:
@@ -551,12 +544,12 @@ def significance_test(sig_rel_file, p_val_cutoff, family_type, tail_type):
 
 
 # get significant relationship file
-# @param        sig_rel_file        significant relationship file
-# @param        p_val cutoff        p value cutoff
-# @param        tail_type           "positive", if p_val < p_val_cutoff and z_val > 0
-#                                   "negative", if p_val < p_val_cutoff and z_val < 0
-#                                   "both",     if p_val < p_val_cutoff
-# @param        e_L                 [e], a list of effects
+# @param        sig_rel_file       significant relationship file
+# @param        p_val cutoff       p value cutoff
+# @param        tail_type          "positive", if p_val < p_val_cutoff and z_val > 0
+#                                  "negative", if p_val < p_val_cutoff and z_val < 0
+#                                  "both",     if p_val < p_val_cutoff
+# @param        e_L                [e], a list of effects
 def get_sig_rel_file(sig_rel_file, p_val_cutoff, tail_type, e_L):
   # get alpha_LL
   alpha_LL = []
